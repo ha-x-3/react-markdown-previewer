98d1d0aaccfc3c8352e2692b6c442396d3dce520
Brittney Epperson <beppers87@hotmail.com>
2024-05-18 13:12:25 -0500

    Adjusted elements to skeleton and added dompurify for security

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { marked } from 'marked';
 
 function App() {
-  const [textArea, setTextArea] = useState('');
+	const [textArea, setTextArea] = useState('');
 
-  return (
-    <>
-      <h1>Markdown Previewer</h1>
-      <textarea id='editor' value={textArea} onChange={e => setTextArea(e.target.value)}></textarea>
-      <div id='preview'>{textArea}</div>
-    </>
-  )
+  const parsedText = (textArea) => {
+    marked.parse(textArea);
+    return parsedText;
+  };
+
+	return (
+		<>
+			<h1>React Markdown Previewer</h1>
+			<div id='editorDiv'>
+        <h2>Editor</h2>
+				<textarea
+					id='editor'
+					value={textArea}
+					onChange={(e) => setTextArea(e.target.value)}
+				></textarea>
+			</div>
+			<div id='previewDiv'>
+        <h2>Previewer</h2>
+				<div id='preview'>{parsedText}</div>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
