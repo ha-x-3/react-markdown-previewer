83a6fd7e01b256fb76bcb547edf8a32c06166dc2
Brittney Epperson <beppers87@hotmail.com>
2024-05-18 14:16:51 -0500

    Styling updates - added Prism.js

--- src/App.jsx
+++ src/App.jsx
@@ -21,18 +21,25 @@ function App() {
 
 	return (
 		<>
-			<h1>React Markdown Previewer</h1>
-			<div id='editorDiv'>
-        <h2>Editor</h2>
-				<textarea
-					id='editor'
-					value={textArea}
-					onChange={(e) => setTextArea(e.target.value)}
-				></textarea>
-			</div>
-			<div id='previewDiv'>
-        <h2>Previewer</h2>
-				<div id='preview' dangerouslySetInnerHTML={{ __html: parsedText(textArea) }}></div>
+			<div id='wrapper'>
+				<h1>React Markdown Previewer</h1>
+				<div id='editorDiv'>
+					<h2 className='heading'>Editor</h2>
+					<textarea
+						id='editor'
+						value={textArea}
+						onChange={(e) => setTextArea(e.target.value)}
+					></textarea>
+				</div>
+				<div id='previewDiv'>
+					<h2 className='heading'>Previewer</h2>
+					<div
+						id='preview'
+						dangerouslySetInnerHTML={{
+							__html: parsedText(textArea),
+						}}
+					></div>
+				</div>
 			</div>
 		</>
 	);
